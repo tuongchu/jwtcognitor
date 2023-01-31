df341316cca86045d367f020538220900b397623
Tuong Chu <tuongcv@gmail.com>
2023-01-31 10:40:48 +0700

    2 commit

---
+++ src/main/java/com/tuongchu/jwtcognitor/JwtcognitorApplication.java
@@ -0,0 +1,13 @@
+package com.tuongchu.jwtcognitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtcognitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtcognitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuongchu/jwtcognitor/JwtcognitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuongchu.jwtcognitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtcognitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
